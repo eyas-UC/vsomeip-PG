1f3d1378c82a46b4eda3c8fcfafa7c89c7ab00b9
eawadelk <eawadelk@jaguarlandrover.com>
2024-05-20 23:44:20 +0100

    add code base

---
+++ code-base/support/config.h
@@ -0,0 +1,12 @@
+#ifndef CONFIG
+#define CONFIG
+#include <stdint.h>
+
+constexpr uint32_t SERVICE_ID {0X1000};
+constexpr uint32_t SERVICE_INSTANCE {0X0001};
+constexpr uint32_t METHOD_ID {0X0001};
+
+
+
+
+#endif
